86ebec5ea7c6aeab7078a73d6d5b59ffa72cd263
Roberto Polli <roberto.polli@par-tec.it>
2015-10-08 17:28:31 +0200

    shard-ready fixed

--- mongodb/tutorial/setup-shard.sh
+++ mongodb/tutorial/setup-shard.sh
@@ -1,15 +1,23 @@
 # Configures shards
-docker-compose scale shard=9
-sleep 20
+docker-compose scale shard=3
+sleep 2
 
-for i in 1 4 7; do
+for i in 1 4 7 ; do
+	primary="tutorial_shard_$i"
 	secondary="tutorial_shard_$((i+1))"
 	arbiter="tutorial_shard_$((i+2))"
 
-	 mongo $(docker inspect -f '{{.NetworkSettings.IPAddress}}' "tutorial_shard_$i") <<< "
-rs.initiate();
+	mongo $(docker inspect -f '{{.NetworkSettings.IPAddress}}' "$primary")  <<< "
+rs.initiate({
+  _id: \"rs\", 
+  members: [ 
+    { _id: 0, host: \"$primary\" }
+  ]
+});
 sleep(3000);
 rs.add(\"$secondary\");
 rs.addArb(\"$arbiter\");
-	"
+"
+
+
 done
